11964ae6d3f87870a019da87ad36d3121a88c428
Joe <joeeldik@hotmail.com>
2018-05-24 18:40:37 -0400

    Add Sample Function

--- main.c
+++ main.c
@@ -3,3 +3,16 @@ int main(void) {
 	printf("Hello, world!\n");
 	return 0;
 }
+
+// Prints matrix
+void printMatrix() {
+	
+}
+
+// Enters move into the matrix.
+// x is position along x axis
+// y is position along y axis\
+// type is x or o
+void enterMove(int x, int y, char type) {
+	
+}
